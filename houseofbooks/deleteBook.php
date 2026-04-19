b0e28cdb386aacf0692b064c4174aab4f3e90ae7
Daniel <gildresdaniel1@gmail.com>
2026-04-19 23:14:14 +0200

    first commit

---
+++ houseofbooks/deleteBook.php
@@ -0,0 +1,15 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "books_db");
+if ($conn->connect_error) die("Error connexió");
+// header('Content-Type: application/json');
+
+$id = $_POST["id"];
+
+$sql = "DELETE FROM books WHERE id='$id'";
+$result = $conn->query($sql);
+if($result != false){
+    echo json_encode(["status"=>"ok"]);
+}
+else{
+    echo json_encode(["status"=>"ko"]);
+}
